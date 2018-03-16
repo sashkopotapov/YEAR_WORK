0f8602b4f5c7f84c0b8500591ed8615ce736d855
oleksandrpotapov <opotapov@ucu.edu.ua>
2018-03-17 00:54:18 +0200

    third commit

---
+++ examples/data_example.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*-
+#simple example of working with json data.
+#This program makee Telegram bot repeat everything you write to him.
+import telebot
+
+token = 'Андрію Богдановичу, тут має бути ключ, але я не пишу його, бо всеодно Вам треба мати доступ до бота в телеграмі,' \
+        'він я його перероблюю, тому він не матиме такого функціоналу, просто повірте, що він працює'
+bot = telebot.TeleBot(token)
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
